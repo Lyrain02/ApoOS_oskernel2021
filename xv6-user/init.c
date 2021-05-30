1e589d33f41ca695c2f10b7495e2ac3a639fb5bb
Lyrain02 <819170128@qq.com>
2021-05-31 03:10:04 +0800

    test

--- xv6-user/init.c
+++ xv6-user/init.c
@@ -2,11 +2,9 @@
 
 #include "xv6-user/user.h"
 
-//const int test_c = 31;
-const int test_c=1;
+const int test_c = 31;
 
-//char *test_name[] = {"fstat","openat","mkdir_","read","write","getcwd","getpid","getppid","open","pipe","fork","dup","dup2","exit","execve","brk","close","clone","chdir","wait","waitpid","yield","getdents","times","umount","uname","unlink","gettimeofday","mmap","mount","munmap"};
-char *test_name[] ={"getdents"};
+char *test_name[] = {"fstat","getdents","openat","mkdir_","read","write","getcwd","getpid","getppid","open","pipe","fork","dup","dup2","exit","execve","brk","close","clone","chdir","wait","waitpid","yield","times","umount","uname","unlink","gettimeofday","mmap","mount","munmap"};
 
 void test(char* name) {
   int pid = fork();
