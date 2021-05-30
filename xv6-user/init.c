5b3b87e5fe13f72bbcbd98f1f64a30bbd8a12b19
YiFan-Evan <61977364+YiFan-Evan@users.noreply.github.com>
2021-05-30 17:50:05 +0800

    vol.2

---
+++ xv6-user/init.c
@@ -0,0 +1,41 @@
+// init: The initial user-level program
+
+#include "xv6-user/user.h"
+
+const int test_c = 31;
+
+char *test_name[] = {"read","write","getcwd","getpid"
+                     ,"getppid","brk","open","pipe"
+                     ,"close","fork","openat","dup"
+                     ,"exit","chdir","execve","getdents"
+                     ,"mkdir_","times","umount","uname"
+                     ,"unlink","clone","wait","waitpid"
+                     ,"yield","fstat","dup2","gettimeofday"
+                     ,"mmap","mount","munmap"
+};
+
+void test(char* name) {
+  int pid = fork();
+  char *argv[2];
+  if (pid == 0) {
+    argv[0] = name;
+    exec(name, argv);
+  } else {
+    wait(-1);
+  }
+}
+
+int
+main(void)
+{
+  dev(O_RDWR, 1, 0);
+  dup(0);
+  dup(0);
+
+  int i;
+  for (i = 0; i < test_c; i++) {
+    test(test_name[i]);
+  }
+
+  for(;;);
+}
