60778340f3a3f8d987e3da51726d6bc48d4de4de
YiFan-Evan <61977364+YiFan-Evan@users.noreply.github.com>
2021-05-30 17:48:39 +0800

    vol.1

--- kernel/sysctl.d
+++
@@ -1,3 +0,0 @@
-kernel/sysctl.o: kernel/sysctl.c kernel/include/types.h \
- kernel/include/sysctl.h kernel/include/types.h \
- kernel/include/memlayout.h
--- xv6-user/initcode.d
+++
@@ -1,1 +0,0 @@
-xv6-user/initcode.o: xv6-user/initcode.S kernel/include/sysnum.h
